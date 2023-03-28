9ecf379f229d5be250a82ea786e200d1ebea3284
David Martinez <david.mtz00@hotmail.com>
2023-03-27 22:22:59 -0500

    Change usage of OpenAI object

--- pages/api/events.js
+++ pages/api/events.js
@@ -35,7 +35,7 @@ function extractUrls(message) {
 async function generateSummary(url) {
     try {
         const prompt = `Please summarize the following article: ${url}`;
-        const response = await openai.Completion.create({
+        const response = await axios.post('https://api.openai.com/v1/completions', {
             model: 'text-davinci-003',
             prompt,
             max_tokens: 100,
