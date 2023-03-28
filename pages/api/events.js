ff082a77a49b406c87a3218bdb1171de809bde78
David Martinez <david.mtz00@hotmail.com>
2023-03-27 20:32:24 -0500

    Check slack signing

--- pages/api/events.js
+++ pages/api/events.js
@@ -4,6 +4,8 @@ import * as OpenAI from 'openai';
 
 OpenAI.apiKey = process.env.OPENAI_API_KEY;
 
+console.log('SLACK_SIGNING_SECRET:', process.env.SLACK_SIGNING_SECRET);
+
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 
